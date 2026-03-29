7f5f46cf2fc5d88de6df0b990c00921d1f67eca4
ahmedfathy112 <ahmedfathy241110@gmail.com>
2026-03-29 22:17:44 +0200

    update in settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
-import contentCollections from '@content-collections/vite'
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import contentCollections from "@content-collections/vite";
+
+// لاحظ أننا حذفنا استيراد netlify
 
 const config = defineConfig({
   plugins: [
     contentCollections(),
     viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
     tailwindcss(),
-    netlify(),
+    // tanstackStart و viteReact يفضل وضعهما في النهاية
+    // لضمان معالجة الملفات بعد الـ تصفية والمسارات
     tanstackStart(),
     viteReact(),
   ],
-})
+});
 
-export default config
+export default config;
